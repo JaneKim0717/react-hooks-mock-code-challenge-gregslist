0fee91d831e22d2327e259fed5fb2d1ded5fe3c5
Jane Kim <jane.kim0717@gmail.com>
2022-03-01 21:00:45 -0500

    Completed Core Deliverables

--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,11 +1,37 @@
-import React from "react";
-// import ListingCard from "./ListingCard";
+import React, { useEffect, useState } from "react";
+import ListingCard from "./ListingCard";
 
-function ListingsContainer() {
+function ListingsContainer({ search }) {
+
+  const [listings, setListings] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:6001/listings')
+    .then(response => response.json())
+    .then(listings => setListings(listings))
+  }, [])
+
+  function handleDeleteListing(id) {
+    const updatedListingsArray = listings.filter(listing => listing.id !== id)
+    setListings(updatedListingsArray)
+  }
+
+  const filteredListings = listings.filter(listing => {
+    return listing.description.toLowerCase().includes(search)
+  })
+
+  const listingCards = filteredListings.map((singleListing) => {
+    return <ListingCard 
+      key={singleListing.id} 
+      listing={singleListing}
+      onDeleteListing={handleDeleteListing}
+    />
+  })
+
   return (
     <main>
       <ul className="cards">
-        {/* use the ListingCard component to display listings */}
+        {listingCards}
       </ul>
     </main>
   );
